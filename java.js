f009404e2ad6f327ace8d692c05bf8727e90053c
Finn Harkrot Berge <finnharkort@gmail.com>
2020-11-17 20:05:42 +0100

    change many vw to rem to work with better responsivity for start, fix title to start animation

--- java.js
+++ java.js
@@ -1,5 +1,8 @@
 let content = document.querySelector("#title");
+let body = document.querySelector("body");
 
 document.querySelector("#grid-link").addEventListener("click", () => {
   content.classList.add("hidden");
+  content.classList.remove("visible");
+  body.classList.add("bodyHidden");
 });
