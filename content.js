f4a72341575cb5ed3196fa7b69d32781772fdb66
Finn Harkrot Berge <finnharkort@gmail.com>
2020-11-20 16:39:20 +0100

    finish all pages and fix last responsivity

--- content.js
+++
@@ -1,8 +0,0 @@
-let contentA = document.querySelector("#title");
-let contentB = document.querySelector(".start-grid-container");
-
-contentBLink.addEventListener("click", () => {
-  contentB.classList.toggle();
-});
-
-let contentBLink = document.querySelector("#grid-link");
